4e0ab52df19455750acf0a2dcbd93aa0559df805
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2019-03-14 18:44:08 -0700

    Marked sgb function as private as well as adding placeholders for save and load

--- src/model.py
+++ src/model.py
@@ -28,6 +28,7 @@ def Conv2DTransposeLReLU(*args, **kwargs):
 
 def Swish(inputs):
     # Do we put a convolution in here?
+    conv =
     layer = tf.nn.swish(inputs)
     layer = tf.layers.MaxPooling2D(pool_size=2, strides=2)(layer)
     return layer
@@ -38,11 +39,11 @@ class SGRU(object):
     def __init__(self, inputs):
         self.inputs = inputs
 
-        inputs, conv1 = self.swish_gated_block('SGB_1', inputs, 96, conv1x1=False)
-        inputs, conv2 = self.swish_gated_block('SGB_2', inputs, 192)
-        inputs, conv3 = self.swish_gated_block('SGB_3', inputs, 288)
-        inputs, conv4 = self.swish_gated_block('SGB_4', inputs, 384)
-        inputs, conv5 = self.swish_gated_block('SGB_5', inputs, 480)
+        inputs, conv1 = self._swish_gated_block('SGB_1', inputs, 96, conv1x1=False)
+        inputs, conv2 = self._swish_gated_block('SGB_2', inputs, 192)
+        inputs, conv3 = self._swish_gated_block('SGB_3', inputs, 288)
+        inputs, conv4 = self._swish_gated_block('SGB_4', inputs, 384)
+        inputs, conv5 = self._swish_gated_block('SGB_5', inputs, 480)
 
         swish1 = Swish(conv1)
         swish2 = Swish(conv2)
@@ -50,11 +51,11 @@ class SGRU(object):
         swish4 = Swish(conv4)
         swish5 = Swish(conv5)
 
-        inputs, _ = self.swish_gated_block('SGB_5_up', inputs, 512, cat=[swish5])
-        inputs, _ = self.swish_gated_block('SGB_4_up', inputs, 480, cat=[swish4])
-        inputs, _ = self.swish_gated_block('SGB_3_up', inputs, 384, cat=[swish3])
-        inputs, _ = self.swish_gated_block('SGB_2_up', inputs, 288, cat=[swish2])
-        inputs, _ = self.swish_gated_block('SGB_1_up', inputs, 192, cat=[swish1])
+        inputs, _ = self._swish_gated_block('SGB_5_up', inputs, 512, cat=[swish5])
+        inputs, _ = self._swish_gated_block('SGB_4_up', inputs, 480, cat=[swish4])
+        inputs, _ = self._swish_gated_block('SGB_3_up', inputs, 384, cat=[swish3])
+        inputs, _ = self._swish_gated_block('SGB_2_up', inputs, 288, cat=[swish2])
+        inputs, _ = self._swish_gated_block('SGB_1_up', inputs, 192, cat=[swish1])
 
         conv1_1_up = Conv2DLReLU(filters=96, kernel_size=1, inputs=inputs)
         conv1_2_up = Conv2DLReLU(filters=96, kernel_size=3, inputs=conv1_1_up)
@@ -71,7 +72,7 @@ class SGRU(object):
     # (Think of a better variable name than cat)
     # If the cat list is an empty list, we assume we are in the down part of the
     # Unet. Otherwise, we are in the up part.
-    def swish_gated_block(self, name, inputs, filters, cat=[], conv1x1=True):
+    def _swish_gated_block(self, name, inputs, filters, cat=[], conv1x1=True):
 
         if conv1x1:
             inputs = Conv2DLReLU(filters=filters, kernel_size=1, inputs=inputs)
@@ -85,3 +86,9 @@ class SGRU(object):
             sgb_op = Conv2DTransposeLReLU(filters=filters, inputs=conv2)
 
         return tf.concat([sgb_op, Swish(inputs)] + cat, axis=3), conv2
+
+    def save(self, path):
+        raise NotImplementedError
+
+    def load(self, path):
+        raise NotImplementedError
